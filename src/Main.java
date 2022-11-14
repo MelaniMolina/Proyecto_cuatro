f66f89de9af3dfc4a2713008e0228883924fcc01
POO <melani.molina@epn.edu.ec>
2022-11-14 11:48:31 -0500

    nuevo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Carros car = new Carros();
 
+        System.out.println("Numero de la matricula: "+car.getMatricula());
+        System.out.println("Año del vehiculo: "+car.getAnio());
+        System.out.println("Modelo del vehiculo: "+car.getModelo());
+        System.out.println("Color del vehiculo: "+car.getColor());
+        System.out.println("");
 
-        System.out.println("Calculadora");
-        System.out.println("Suma: ");
-        System.out.println("Resta: ");
-        System.out.println("Multiplicacion: ");
-        System.out.println("Divsion: ");
-
-
-
+        System.out.println("\t Tipos de Musica");
+        Musica mu = new Musica();
+        System.out.println("Nombre de la Cancion: "+mu.getNom_can());
+        System.out.println("Nombre del Artista: "+mu.getNom_artis());
+        System.out.println("Año de Estreno: "+mu.getAnio());
 
     }
 }
